6205f832ef9b8f214fa697c608cc52e8a2228097
Abdubositkhon Khabirov <xabirovbob@gmail.com>
2025-03-24 19:42:53 +0100

    Change Font

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,11 @@
 import Logo from './_components/Logo';
 import Navigation from './_components/Navigation';
+import { Josefin_Sans } from 'next/font/google';
+
+const josefin = Josefin_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 import '@/app/_styles/globals.css';
 
@@ -15,7 +21,9 @@ export const metadata = {
 function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className='bg-primary-950 text-primary-100 min-h-screen'>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navigation />
