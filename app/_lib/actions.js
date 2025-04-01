033620dbd87273855f30244b4bc85bcda3d197e8
Abdubositkhon Khabirov <xabirovbob@gmail.com>
2025-04-01 16:13:05 +0200

    Delete Reservation, Update a reservation  and Include loading indicator

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,8 @@
 import { revalidatePath } from 'next/cache';
 import { auth, signIn, signOut } from './auth';
 import { supabase } from './supabase';
+import { getBookings } from './data-service';
+import { redirect } from 'next/navigation';
 
 export async function updateGuest(formData) {
   const session = await auth();
@@ -28,6 +30,62 @@ export async function updateGuest(formData) {
   if (error) throw new Error('Guest could not be updated');
 
   revalidatePath('/account');
+}
+
+export async function updateReservation(formData) {
+  const bookingId = Number(formData.get('bookingId'));
+
+  // Authentication
+  const session = await auth();
+  if (!session) throw new Error('You must be logged in');
+
+  // Authorization
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingsIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingsIds.includes(bookingId))
+    throw new Error('You are not allowed to update this booking');
+
+  // Build update data
+  const numGuests = String(formData.get('numGuests'));
+  const observations = formData.get('observations').slice(0, 1000);
+  const updateData = { numGuests, observations };
+
+  // Mutation
+  const { error } = await supabase
+    .from('bookings')
+    .update(updateData)
+    .eq('id', bookingId)
+    .select()
+    .single();
+
+  // Error handling
+  if (error) throw new Error('Reservation could not be updated');
+
+  // Revalidate and redirect
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+  redirect('/account/reservations');
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error('You must be logged in');
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingsIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingsIds.includes(bookingId))
+    throw new Error('You are not allowed to delete this booking');
+
+  const { error } = await supabase
+    .from('bookings')
+    .delete()
+    .eq('id', bookingId);
+
+  if (error) throw new Error('Booking could not be deleted');
+
+  // TAG - there are more ways to revalidate, e.g. Tag. Check out the docs.
+  revalidatePath('/account/reservations');
 }
 
 export async function signInAction() {
